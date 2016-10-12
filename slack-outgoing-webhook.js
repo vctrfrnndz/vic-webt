c60855c33b4785c9e1728a732c340459b6f183ce
Victor Fernandez <victorhf@gmail.com>
2016-10-12 11:40:45 -0300

    Create slack-outgoing-webhook.js

---
+++ slack-outgoing-webhook.js
@@ -0,0 +1,13 @@
+// Slack will send a request for each message sent on any or a specific channel.
+// If trigger word has been configured on Slack, only messages starting with
+// that trigger word will be sent
+
+module.exports = function (context, done) {
+  console.log('slack request: ', context.data);
+  
+  console.log(context.secrets);
+  
+  done(null, { text: 'hello pileta' });
+  
+  
+};
